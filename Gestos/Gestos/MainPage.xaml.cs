5eadae669cee690dbb830c1a5e28f0f10eff662d
htorricotecsup <53871912+htorrico@users.noreply.github.com>
2022-10-25 11:38:53 -0500

    Add project files.

---
+++ Gestos/Gestos/MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Gestos
+{
+    public partial class MainPage : ContentPage
+    {
+        bool isVisible = false;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //Crear mi gesto
+            var tap1 = new TapGestureRecognizer();
+            //Asigno el evento a mi gesto
+            tap1.Tapped += EventoTap;
+            //Imagen le agrego mi  gesto
+            image1.GestureRecognizers.Add(tap1);
+
+
+            var tap2 = new TapGestureRecognizer();
+            //Asigno el evento a mi gesto
+            tap2.Tapped += EventoTap2;
+
+            this.bvExample.GestureRecognizers.Add(tap2);
+
+
+        }
+
+        void EventoTap(object sender, EventArgs args)
+        {
+            var imageSender = (Image)sender;
+            isVisible = !isVisible;
+            if (isVisible)
+            {
+                imageSender.Source = "tapped.jpg";
+            }
+            else
+            {
+                imageSender.Source = "tapped2.jpg";
+            }
+
+            labelMessage.Text = "Ejemplo";
+        }
+        void EventoTap2(object sender, EventArgs args)
+        {
+            
+            isVisible = !isVisible;
+            if (isVisible)
+            {
+                image1.Source = "tapped.jpg";
+            }
+            else
+            {
+                image1.Source = "tapped2.jpg";
+            }
+
+            
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Gestos/Gestos/Page1.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Gestos
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        void OnSwiped(object sender, SwipedEventArgs e)
+        {
+            string direction = e.Direction.ToString();
+
+            if (direction == "Right")
+                this.bvDemo.BackgroundColor = Color.Red;
+            if (direction == "Left")
+                this.bvDemo.BackgroundColor = Color.Yellow;
+            if (direction == "Up")
+                this.bvDemo.BackgroundColor = Color.Orange;
+            if (direction == "Down")
+                this.bvDemo.BackgroundColor = Color.Green;
+
+
+
+
+        }
+
+    }
+}
